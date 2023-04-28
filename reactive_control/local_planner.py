7c3e9cdda0f8e6bcfa11e146eb5485f42e4ddee7
holmes <holmes>
2023-04-28 20:28:48 +0800

    0428_msi_1

--- reactive_control/local_planner.py
+++ reactive_control/local_planner.py
@@ -8,23 +8,28 @@
 #small brain or main brain?
 import numpy as np
 import sys
-sys.path.append("../")
+sys.path.append("/home/holmes/Desktop/graduation/code/graduation_simulation_code")
 # print(sys.path)
 import matplotlib.pyplot as plt
 from quadprog import solve_qp
 from numpy.linalg import matrix_rank as rank,inv
 from traj_optimization.traj import traj_opt,traj_show
-
+from math import sqrt
+import local_planner_conf as conf
 def nt(t):
     return np.array([pow(t,5),pow(t,4),pow(t,3),pow(t,2),t,1]) 
+def dnt(t):
+    return np.array([5*pow(t,4),4*pow(t,3),3*pow(t,2),2*t,1,0]) 
+def ddnt(t):
+    return np.array([20*pow(t,3),12*pow(t,2),6*t,2,0,0]) 
+
+
 def mnt(t):
     return np.block([
         [nt(t),np.zeros(6),np.zeros(6)],
         [np.zeros(6),nt(t),np.zeros(6)],
         [np.zeros(6),np.zeros(6),nt(t)]])
 
-def dnt(t):
-    return np.array([5*pow(t,4),4*pow(t,3),3*pow(t,2),2*t,1,0]) 
 
 def mdnt(t):
     return np.block([
@@ -32,62 +37,118 @@ def mdnt(t):
         [np.zeros(6),dnt(t),np.zeros(6)],
         [np.zeros(6),np.zeros(6),dnt(t)]])
 
-
-def ddnt(t):
-    return np.array([20*pow(t,3),12*pow(t,2),6*t,2,0,0]) 
-
+ 
 def mddnt(t):
     return np.block([
         [ddnt(t),np.zeros(6),np.zeros(6)],
         [np.zeros(6),ddnt(t),np.zeros(6)],
         [np.zeros(6),np.zeros(6),ddnt(t)]])
 
+
+def mT(t):
+    return np.vstack([nt(0),dnt(0),ddnt(0),nt(t),dnt(t),ddnt(t)])
+
+def T_all(t):
+    return np.vstack([nt(t),dnt(t),ddnt(t)])
 """
     for swing foot motion, we get some assumption like this
     1. the velocity and accerlation of the start point and end point are zero
     2. the velocity of the apex in the trajectory is set to [1.,0.,0.],suppose the robot just move in x direction
+    3. for now the robot can not jump because the z direction
 """
-def traj_2seg_spline(p_s,p_e,T,v):
-    
-    p_m  = 0.5*(p_s + p_e)+np.array([0.,0.,0.03])
-    dp_m =np.array([2.0*v[0],0.,0.])
+def swing_foot_plan(p_s,p_e,T):
+    z = 0.02
+    A = mT(T)
+    A_inv = inv(A)
+    Az = mT(T/2.0)
+    Az_inv = inv(Az) 
+    coeff0 = A_inv[:,0]*p_s[0]+A_inv[:,3]*p_e[0]
+    coeff1 = A_inv[:,0]*p_s[1]+A_inv[:,3]*p_e[1]
+    coeff2_1 = Az_inv[:,3]*z
+    coeff2_2 = Az_inv[:,0]*z
+    return coeff0,coeff1,np.hstack([coeff2_1,coeff2_2])
 
-    dp_s =np.zeros(3)
-    dp_e =np.zeros(3)
+def swing_foot_traj_get(t,T,coeff0,coeff1,coeff2):
+    x = T_all(t)@coeff0
+    y = T_all(t)@coeff1
+    z = T_all(t)@coeff2[:6] if t < T/2 else T_all(t-T/2)@coeff2[6:]
+    return np.array([x[0],y[0],z[0]]),np.array([x[1],y[1],z[1]]),np.array([x[2],y[2],z[2]])
 
-    ddp_e =np.zeros(3)
-    ddp_s =np.zeros(3)
-    ddp_m =np.zeros(3)
-    
-    A = np.vstack([mnt(0),mdnt(0),mddnt(0),mnt(T),mdnt(T),mddnt(T)])
-    A_ = np.block([[A,np.zeros(A.shape)],[np.zeros(A.shape),A]])
-    b = np.hstack([p_s,dp_s,ddp_s,p_m,dp_m,ddp_m,p_m,dp_m,ddp_m,p_e,dp_e,ddp_e])
-    G = A_.T@A_
-    h = A_.T@b
-    xf, f, xu, iters, lagr, iact = solve_qp(G,h)
-    return xf
+def plot_convex_shape(vertices, color='k'):
+    """Plot a convex shape given its vertices using matplotlib."""
+    for i in range(len(vertices)-1,-1,-1):
+        if vertices[i] is None:
+            vertices.pop(i)
+    num_vertices = len(vertices)
+    x = [vertices[i][0] for i in range(num_vertices)]
+    y = [vertices[i][1] for i in range(num_vertices)]
+    # plt.fill(x, y, color=color)
+    x.append(vertices[0][0])  # Add the first vertex to close the shape
+    y.append(vertices[0][1])  # Add the first vertex to close the shape
+    plt.plot(x, y, color=color)
+
+
+def reduce_convex(polygon_set,s=0.025,w=0.025):
+    """reduce the shape of the support polygons"""
+    reduce_polygon = []
+    #get the normal vector and the bias of each edge
+    for [vertex,duration] in polygon_set:
+        vertex_ = []
+        new_vertex = []
+        for k in vertex:
+            if k is not None:
+                new_vertex.append(k.copy())
+                vertex_.append(k.copy())
+        num_vertices = len(new_vertex)
+        if num_vertices>2:
+            c = np.zeros(num_vertices)
+            vec_n = np.zeros((num_vertices,2))
+            for i in range(num_vertices):
+                direct = vertex_[(i + 1) % num_vertices] - vertex_[i]
+                normal = np.array([direct[1], -direct[0]])#inside polygons direct
+                normal /= np.linalg.norm(normal)
+                new_vertex[i] +=s*normal
+                vec_n[i] = normal
+                c[i] = -normal@new_vertex[i]
+                # new_vertex[(i + 1) % num_vertices] +=s*normal
+            for i in range(num_vertices):
+                new_vertex[i] = -np.linalg.inv(vec_n[[i,(i+1)% num_vertices]])@c[[i,(i+1)% num_vertices]]
+            reduce_polygon.append((new_vertex,duration))
+        else:
+            direct = new_vertex[1] - new_vertex[0]
+            direct /=np.linalg.norm(direct)
+            normal = np.array([direct[1], -direct[0]])#inside polygons direct
+            normal /= np.linalg.norm(normal)
+            v1 = new_vertex[0]+w*normal+s*direct
+            v2 = new_vertex[1]+w*normal-s*direct
+            v3 = new_vertex[1]-w*normal-s*direct
+            v4 = new_vertex[0]-w*normal+s*direct
+            reduce_polygon.append(([v1,v2,v3,v4],duration))
+    return reduce_polygon
 
 
 class local_planner:
-    time_factor = 1.0 # the time of a period of motion
-    contact = [True,True,True,True]# [FL,FR, RL,RR] follow this sequence, True is in contact, False means in swing phase
-    phase = [0,0,0,0]#  when in contact the phase decreasing for 0.75 to 0, when in swing the phase increasing for 0 to 0.25 seconds have 
+    contact = [True,True,True,True]# [FL,FR, RR,RL] follow this sequence, True is in contact, False means in swing phase
     first_stand = [True,True,True,True]
     first_swing = [False,False,False,False]
-    next_foot =np.zeros((4,3))
-    swing_coeff = np.zeros((4,36))
     #change the force by the phase
-    def __init__(self,conf,T,dt):
+    def __init__(self,conf,T):
         self.stance_phase = 0.75# the phase of stance 
         self.swing_phase = 0.25# the time of swing 
-        self.lift_off = np.array([0.05,0.55,0.7,0.2])# the lift off event time!
+        self.lift_off = np.array([0.05,0.55,0.2,0.7])# the lift off event time!
         self.touch_down = self.lift_off+self.swing_phase
         self.touch_down *=T
         self.lift_off *=T
+        self.stance_phase *=T# the phase of stance 
+        self.swing_phase *=T# the time of swing 
+        self.T = T
         self.conf = conf
-        self.N = T/dt
-        self.iter_time = np.linspace(0,T,N) 
-        self.cur = 0
+        self.cur = 0.0
+        self.phase = np.zeros(4)#  when in contact the phase decreasing for 0.75 to 0, when in swing the phase increasing for 0 to 0.25 seconds have 
+        self.next_foot =np.zeros((4,2))
+        self.coeff0 = np.zeros((4,6))
+        self.coeff1 = np.zeros((4,6))
+        self.coeff2 = np.zeros((4,12))
         return
     
     
@@ -96,21 +157,22 @@ class local_planner:
     """
     foot: np.array, shape(3,4)
     """
-    def update(self):
-        self.cur +=1
-        if self.cur>=N:
-            self.cur = 0
+    def update(self,dt,foot,hip,v_ref,v_hip):
+        self.cur +=dt
+        if self.cur>self.T:
+            self.cur = 0.0
         for i in range(4):
-            if self.current_phi > self.lift_off[i] and self.current_phi < self.touch_down[i]:
+            if self.cur > self.lift_off[i] and self.cur < self.touch_down[i]:
                 if self.first_swing[i] :
                     #do some thing
                     #2. get the coefficient of the swing trajectory 
-                    # self.swing_coeff[i] = traj_2seg_spline(foot[i],self.next_foot[i],self.swing_phase*self.time_factor*0.5,v)
                     #plan
+                    self.coeff0[i],self.coeff1[i],self.coeff2[i] = swing_foot_plan(foot[i],self.next_foot[i],self.swing_phase)
                     self.first_swing[i]  = False
                 self.contact[i] = False
                 self.first_stand[i] = True
-                self.phase[i] = (self.current_phi - self.lift_off[i])*self.time_factor#in time (second)
+                self.phase[i] = (self.cur - self.lift_off[i])#in time (second)
+
             else:
                 self.contact[i] = True
                 if self.first_stand[i] :
@@ -119,10 +181,10 @@ class local_planner:
                     #plan
                     self.first_stand[i]  = False
                 self.first_swing[i] = True
-                # self.next_foot[i] = np.array([hip[i][0]+1.5*v[0]*self.stance_phase*self.time_factor/2.0,hip[i][1]+v[1]*self.stance_phase*self.time_factor/2.0,foot[i][2]])
-                self.phase[i] = (self.time_factor-self.current_phi+self.lift_off[i])*self.time_factor \
-                    if self.current_phi > self.lift_off[i] else (self.lift_off[i]-self.current_phi)*self.time_factor
-        
+                self.next_foot[i] = np.array([hip[i][0]+v[0]*self.stance_phase/2.0,hip[i][1]+v[1]*self.stance_phase/2.0])
+                self.phase[i] = (self.T-self.cur+self.lift_off[i]) \
+                    if self.cur > self.lift_off[i] else (self.lift_off[i]-self.cur)
+
 
     def in_contact(self,leg):
         return self.contact[leg]
@@ -134,32 +196,60 @@ class local_planner:
                 n-=1
         return n
 
-    def swing_foot_traj_pos(self,leg):
-        if(self.phase[leg]<self.swing_phase*self.time_factor*0.5):
-            return mnt(self.phase[leg])@self.swing_coeff[leg,:18]
-        else:
-            return mnt(self.phase[leg]%(self.swing_phase*self.time_factor*0.5))@self.swing_coeff[leg,18:]
+    def swing_foot_traj(self,leg):
+        p,v,a = swing_foot_traj_get(self.phase[leg],self.swing_phase,self.coeff0[leg],self.coeff1[leg],self.coeff2[leg])
+        return p
         
-
-    def swing_foot_traj_vel(self,leg):
-        if(self.phase[leg]<self.swing_phase*self.time_factor*0.5):
-            return mdnt(self.phase[leg])@self.swing_coeff[leg,:18]
-        else:
-            return mdnt(self.phase[leg]%(self.swing_phase*self.time_factor*0.5))@self.swing_coeff[leg,18:]
-        
-
-
-    def swing_foot_traj_acc(self,leg):
-        if(self.phase[leg]<self.swing_phase*self.time_factor*0.5):
-            return mddnt(self.phase[leg])@self.swing_coeff[leg,:18]
-        else:
-            return mddnt(self.phase[leg]%(self.swing_phase*self.time_factor*0.5))@self.swing_coeff[leg,18:]
-        
+    #event based
+    def get_support_polygon(self,foot,next_foot):
+        t = 0.0
+        a = list(self.lift_off)
+        b = list(self.touch_down)
+        origin_a = a.copy()
+        origin_b = b.copy()
+        support_polygon = []
+        foot_ = list(foot)
+        while len(a)>0 or len(b)>0:
+            if len(a)>0 and len(b)>0:
+                a_min = min(a)
+                b_min = min(b)
+                if a_min < b_min :
+                    index_ = origin_a.index(a_min)
+                    a.remove(a_min )
+                    dt = a_min-t
+                    t = a_min
+                    support_polygon.append([foot_.copy(),dt])
+                    foot_[index_] = None
+                else:
+                    index_ = origin_b.index(b_min)
+                    b.remove(b_min)
+                    dt = b_min-t
+                    t = b_min
+                    support_polygon.append([foot_.copy(),dt])
+                    foot_[index_] = next_foot[index_,:]
+            elif len(a)>0:
+                a_min = min(a)
+                index_ = origin_a.index(a_min)
+                a.remove(a_min )
+                dt = a_min-t
+                t = a_min
+                support_polygon.append([foot_.copy(),dt])
+                foot_[index_] = None
+            elif len(b)>0:
+                b_min = min(b)
+                index_ = origin_b.index(b_min)
+                b.remove(b_min)
+                dt = b_min-t
+                t = b_min
+                support_polygon.append([foot_.copy(),dt])
+                foot_[index_] = next_foot[index_,:]
+        support_polygon.append([foot_.copy(),T-t])
+        return support_polygon
 
 
     def print(self):
         n = self.contact_num()
-        print(str(self.current_phi)+"\n",n,self.contact,self.phase)
+        print(str(self.cur)+"\n",n,self.contact,self.phase)
     
 
     def body_traj_plan(self):
@@ -167,7 +257,7 @@ class local_planner:
         self.cum_duration = np.cumsum(self.duration)
         self.traj_tot_time = sum(self.duration)
         self.dim  = 2
-        #traj_opt(n_seg,dim,duration,stp,dstp,ddstp,fp,p=None,dp=None,ddp=None):
+        #traj_opt(n_seg,dim,duration,stp,dstp,ddstp,fp,p=None,v=None,a=None):
         self.coeff = traj_opt(self.duration,[0,0],[0,0],[0,0],[2.5,.0])
         self.traj_time = 0
         
@@ -192,20 +282,141 @@ class local_planner:
         traj_show(self.duration,self.dim,self.coeff)
 
 
+def print_each_support_polygon(polys_1,polys_2):
+        for i in range(len(polys_1)):
+            plt.figure()
+            plot_convex_shape(polys_1[i][0])
+            plot_convex_shape(polys_2[i][0],'r')
+            plt.grid()
+            plt.xlim([-0.2,0.2])
+            plt.ylim([-0.2,0.2])
+
+
+def print_all_support_polygon(polys_1,polys_2):
+        plt.figure()
+        for i in range(len(polys_1)):
+            plot_convex_shape(polys_1[i][0])
+            plot_convex_shape(polys_2[i][0],'b')
+            plt.grid()
+            plt.xlim([-0.2,0.2])
+            plt.ylim([-0.2,0.2])
+
 
 if __name__ == "__main__":
-    import main_1_conf as conf
     import time
-    local_plan  = local_planner (conf,1,conf.dt)
-    N = int(conf.T_SIMULATION/conf.dt)      
+    N = int(conf.T_SIMULATION/conf.dt)+1  
+    N = 1    
     PRINT_N = int(conf.PRINT_T/conf.dt)
     t = 0.0
     #
-    for ss in range(0, N):#ss: simualtion step
-        time_start = time.time()
-        # local_plan.update(conf.dt,p_f,p_h,dx_bp)
+    T = 1
+    local_plan  = local_planner (conf,T)
+    foot = None
+    hip = None
+    v_ref= None
+    v_hip= None
+    discrete_time = np.array(range(N))*conf.dt
+    #
+    n_dim = 2
+    pb   = np.zeros((n_dim,N))
+    dpb  = np.zeros((n_dim,N))
+    ddpb = np.zeros((n_dim,N))
+    foot_data   = np.zeros((4,n_dim,N))
+    #
+    p = np.zeros(n_dim)
+    v = np.zeros(n_dim)
+    a = np.zeros(n_dim)
+    p0 = np.array([0.0,0.])
+    p_end = np.array([1.,0.])
+    #
+    p = p0
+    #
+    kp = 100
+    kd = 2*sqrt(kp)
+    time_start = time.time()
+    hip = np.zeros((4,2))
+    #FL 0
+    #FR 1 
+    #RR 3
+    #RL 4
+    bias = np.array([[0.2,0.1],
+                     [0.2,-0.1],
+                     [-0.2,-0.1],
+                     [-0.2,0.1]])
+    for i in range(4):
+        hip[i,:] = p+bias[i,:]
+    foot = hip.copy()
+    for ss in range(N):
+        #record the data
+        pb[:,ss] = p
+        dpb[:,ss] = v
+        ddpb[:,ss] = a
+        foot_data[:,:,ss]=foot.copy()
+        #get some point
+        for i in range(4):
+                hip[i,:] = p+bias[i,:]
+        #
+        local_plan.update(conf.dt,foot,hip,v,v)
+        for j in range(4) :
+            if local_plan.in_contact(j):
+                pass 
+            else:
+                foot[j,:] = local_plan.swing_foot_traj(j)[:2]
 
-        if ss%PRINT_N == 0:
-            print("Time %.3f"%(t))
+
+        # 1. plot the convex polygon , done
+        # 2. add the ZMP dynamic Model and get the coefficients
+        support_polygon = local_plan.get_support_polygon(foot,local_plan.next_foot)
+   
+        #simplified model,use pd forward controller that can follow the traj
+        a = kp*(p_end-p)+kd*(-v)
+        p += v*conf.dt
+        v += a*conf.dt
         t += conf.dt
+    print("time eslaped",time.time()-time_start)
+    a = reduce_convex(support_polygon)
+    print_each_support_polygon(support_polygon,a)
+    print_all_support_polygon(support_polygon,a)
+    # # figure p
+    # plt.figure()
+    # plt.plot(discrete_time,pb[0],label='p_x')
+    # plt.plot(discrete_time,pb[1],label='p_y')
+    # plt.grid()
+    # plt.legend()
+    # #figure v
+    # plt.figure()
+    # plt.plot(discrete_time,dpb[0],label='v_x')
+    # plt.plot(discrete_time,dpb[1],label='v_y')
+    # plt.grid()
+    # plt.legend()
+    # #figure a
+    # plt.figure()
+    # plt.plot(discrete_time,ddpb[0],label='a_x')
+    # plt.plot(discrete_time,ddpb[1],label='a_y')
+    # plt.grid()
+    # plt.legend()
+    # #figure 2
+    # plt.figure()
+    # plt.plot(pb[0],pb[1],label='pos')
+    # plt.grid()
+    # plt.legend()
+    # #foot
+    # plt.figure()
+    # plt.plot(discrete_time,foot_data[0,0],label='FL_f_x')
+    # plt.plot(discrete_time,foot_data[0,1],label='FL_f_y')
+    # plt.plot(discrete_time,foot_data[1,0],label='FR_f_x')
+    # plt.plot(discrete_time,foot_data[1,1],label='FR_f_y')
+    # plt.plot(discrete_time,foot_data[2,0],label='RR_f_x')
+    # plt.plot(discrete_time,foot_data[2,1],label='RR_f_y')
+    # plt.plot(discrete_time,foot_data[3,0],label='RL_f_x')
+    # plt.plot(discrete_time,foot_data[3,1],label='RL_f_y')
+    # plt.grid()
+    # plt.legend()
+    # #
+    # plt.figure()
+    # plt.plot(foot_data[0,0],foot_data[0,1],label='FL_foot')
+    # plt.grid()
+    # plt.legend()
+    plt.show()
+
 
--- traj_optimization/test_2.py
+++ traj_optimization/test_2.py
@@ -4,7 +4,7 @@ from quadprog import solve_qp
 from numpy.linalg import matrix_rank as rank,inv
 from numpy import hstack,vstack
 from plot_polygon import plot_convex_shape
-from traj_optimization.traj import traj_opt,traj_show
+
 
 def Acc(T,alpha=1e-8):
     return np.array([[400.0/7.0*pow(T,7),40*pow(T,6),120.0/5.0*pow(T,5),10*pow(T,4),0,0],
@@ -64,17 +64,24 @@ min 1/2 x^T G x  -a^Tx
 s.t. 
     C.T x>= b
 """
+#value and address
 def reduce_convex(polygon_set,s=0.1,w=0.05):
     """reduce the shape of the support polygons"""
     reduce_polygon = []
-    for (vertex,duration) in polygon_set:
-        num_vertices = len(vertex)
-        new_vertex = vertex.copy()
+    #get the normal vector and the bias of each edge
+    for [vertex,duration] in polygon_set:
+        vertex_ = []
+        new_vertex = []
+        for k in vertex:
+            if k is not None:
+                new_vertex.append(k.copy())
+                vertex_.append(k.copy())
+        num_vertices = len(new_vertex)
         if num_vertices>2:
             c = np.zeros(num_vertices)
             vec_n = np.zeros((num_vertices,2))
             for i in range(num_vertices):
-                direct = vertex[(i + 1) % num_vertices] - vertex[i]
+                direct = vertex_[(i + 1) % num_vertices] - vertex_[i]
                 normal = np.array([direct[1], -direct[0]])#inside polygons direct
                 normal /= np.linalg.norm(normal)
                 new_vertex[i] +=s*normal
@@ -85,54 +92,90 @@ def reduce_convex(polygon_set,s=0.1,w=0.05):
                 new_vertex[i] = -np.linalg.inv(vec_n[[i,(i+1)% num_vertices]])@c[[i,(i+1)% num_vertices]]
             reduce_polygon.append((new_vertex,duration))
         else:
-            direct = vertex[1] - vertex[0]
+            direct = new_vertex[1] - new_vertex[0]
             direct /=np.linalg.norm(direct)
             normal = np.array([direct[1], -direct[0]])#inside polygons direct
             normal /= np.linalg.norm(normal)
-            v1 = vertex[0]+w*normal+s*direct
-            v2 = vertex[1]+w*normal-s*direct
-            v3 = vertex[1]-w*normal-s*direct
-            v4 = vertex[0]-w*normal+s*direct
+            v1 = new_vertex[0]+w*normal+s*direct
+            v2 = new_vertex[1]+w*normal-s*direct
+            v3 = new_vertex[1]-w*normal-s*direct
+            v4 = new_vertex[0]-w*normal+s*direct
             reduce_polygon.append(([v1,v2,v3,v4],duration))
     return reduce_polygon
 
 
-def get_line():
-    
-    return 
+def plot_convex_shape(vertices, color='k'):
+    """Plot a convex shape given its vertices using matplotlib."""
+    for i in range(len(vertices)-1,-1,-1):
+        if vertices[i] is None:
+            vertices.pop(i)
+    num_vertices = len(vertices)
+    x = [vertices[i][0] for i in range(num_vertices)]
+    y = [vertices[i][1] for i in range(num_vertices)]
+    # plt.fill(x, y, color=color)
+    x.append(vertices[0][0])  # Add the first vertex to close the shape
+    y.append(vertices[0][1])  # Add the first vertex to close the shape
+    plt.plot(x, y, color=color)
 
 dx = 0.2
 dy = 0.2
 #leg sequence is changed, and the  3rd column is the RR leg, and the 4th column is the RF leg
-polygons = [(np.array([[0.5,   0.25],[0.5   ,-0.25] ,[-0.5   ,-0.25],[-0.5   ,0.25]]),0.05),#0.05-0.0 all down until LF lift
-            (np.array([              [0.5   ,-0.25] ,[-0.5   ,-0.25],[-0.5   ,0.25]]),0.15),#0.2-0.05,LF lift until RR lift
-            (np.array([              [0.5   ,-0.25]                 ,[-0.5   ,0.25]]),0.10),#0.3-0.2 RR lift until LF touch
-            (np.array([[0.5+dx,0.25],[0.5   ,-0.25]                 ,[-0.5   ,0.25]]),0.15),#0.45-0.3LF touch until RR touch
-            (np.array([[0.5+dx,0.25],[0.5   ,-0.25] ,[-0.5+dx,-0.25],[-0.5   ,0.25]]),0.10),#0.55-0.45 all touch until LR lift
-            (np.array([[0.5+dx,0.25],                [-0.5+dx,-0.25],[-0.5   ,0.25]]),0.15),#0.70-0.55 FR lift until RL lift
-            (np.array([[0.5+dx,0.25],                [-0.5+dx,-0.25]               ]),0.10),#0.80-0.70 RL lift until FR touch
-            (np.array([[0.5+dx,0.25],[0.5+dx,-0.25] ,[-0.5+dx,-0.25]               ]),0.15),#0.95-0.8FR touch until RL touch
-            (np.array([[0.5+dx,0.25],[0.5+dx,-0.25] ,[-0.5+dx,-0.25],[-0.5+dx,0.25]]),0.05)]#1.0 -0.95 RL touch until next sequnece
+# polygons = [(np.array([[0.5,   0.25],[0.5   ,-0.25] ,[-0.5   ,-0.25],[-0.5   ,0.25]]),0.05),#0.05-0.0 all down until LF lift
+#             (np.array([              [0.5   ,-0.25] ,[-0.5   ,-0.25],[-0.5   ,0.25]]),0.15),#0.2-0.05,LF lift until RR lift
+#             (np.array([              [0.5   ,-0.25]                 ,[-0.5   ,0.25]]),0.10),#0.3-0.2 RR lift until LF touch
+#             (np.array([[0.5+dx,0.25],[0.5   ,-0.25]                 ,[-0.5   ,0.25]]),0.15),#0.45-0.3LF touch until RR touch
+#             (np.array([[0.5+dx,0.25],[0.5   ,-0.25] ,[-0.5+dx,-0.25],[-0.5   ,0.25]]),0.10),#0.55-0.45 all touch until LR lift
+#             (np.array([[0.5+dx,0.25],                [-0.5+dx,-0.25],[-0.5   ,0.25]]),0.15),#0.70-0.55 FR lift until RL lift
+#             (np.array([[0.5+dx,0.25],                [-0.5+dx,-0.25]               ]),0.10),#0.80-0.70 RL lift until FR touch
+#             (np.array([[0.5+dx,0.25],[0.5+dx,-0.25] ,[-0.5+dx,-0.25]               ]),0.15),#0.95-0.8FR touch until RL touch
+#             (np.array([[0.5+dx,0.25],[0.5+dx,-0.25] ,[-0.5+dx,-0.25],[-0.5+dx,0.25]]),0.05)]#1.0 -0.95 RL touch until next sequnece
 
-
-
-
-"""
-    changed a lot, will be improved 
-"""
-duration=[polygons[j][1] for j in range(len(polygons))]
-c_point = [sum(polygons[j][0])/4 for j in range(len(polygons))]
-duration_=duration[:2]
-stp=[0,2]
-dstp=[0.,0.]
-ddstp=[0.,0.]
-fp=[1,0.5]
-p1=[0.5,1.25]
+polygons = [[[[0.5,   0.25],[0.5   ,-0.25] ,[-0.5   ,-0.25],[-0.5   ,0.25]],0.05],#0.05-0.0 all down until LF lift
+            [[None         ,[0.5   ,-0.25] ,[-0.5   ,-0.25],[-0.5   ,0.25]],0.15],#0.2-0.05,LF lift until RR lift
+            [[None         ,[0.5   ,-0.25] ,None           ,[-0.5   ,0.25]],0.10],#0.3-0.2 RR lift until LF touch
+            [[[0.5+dx,0.25],[0.5   ,-0.25] ,None           ,[-0.5   ,0.25]],0.15],#0.45-0.3LF touch until RR touch
+            [[[0.5+dx,0.25],[0.5   ,-0.25] ,[-0.5+dx,-0.25],[-0.5   ,0.25]],0.10],#0.55-0.45 all touch until LR lift
+            [[[0.5+dx,0.25],None           ,[-0.5+dx,-0.25],[-0.5   ,0.25]],0.15],#0.70-0.55 FR lift until RL lift
+            [[[0.5+dx,0.25],None           ,[-0.5+dx,-0.25],None          ],0.10],#0.80-0.70 RL lift until FR touch
+            [[[0.5+dx,0.25],[0.5+dx,-0.25] ,[-0.5+dx,-0.25],None          ],0.15],#0.95-0.8FR touch until RL touch
+            [[[0.5+dx,0.25],[0.5+dx,-0.25] ,[-0.5+dx,-0.25],[-0.5+dx,0.25]],0.05]]#1.0 -0.95 RL touch until next sequnece
+#modified data structure
+for i in range(len(polygons)):
+    for j in range(len(polygons[i][0])):
+        if polygons[i][0][j] is not None:
+            polygons[i][0][j] = np.array(polygons[i][0][j])
+        else:
+            polygons[i][0][j] = None
 #
-n_seg = 2 #number of segments
-dim = 2 #number of coordinates
-delta = 0.01
+a = reduce_convex(polygons)
+print(polygons[2])
+for i in range(len(polygons)):
+    plt.figure()
+    plot_convex_shape(polygons[i][0])
+    plot_convex_shape(a[i][0],'r')
+    plt.grid()
+    plt.xlim([-1,1])
+    plt.ylim([-1,1])
 
-regular_coeff =traj_opt(duration_,stp,dstp,ddstp,fp)
+# for i in range(len(a)):
 
-traj_show(duration_,len(stp),regular_coeff)
+plt.show()
+# """
+#     changed a lot, will be improved 
+# """
+# duration=[polygons[j][1] for j in range(len(polygons))]
+# c_point = [sum(polygons[j][0])/4 for j in range(len(polygons))]
+# duration_=duration[:2]
+# stp=[0,2]
+# dstp=[0.,0.]
+# ddstp=[0.,0.]
+# fp=[1,0.5]
+# p1=[0.5,1.25]
+# #
+# n_seg = 2 #number of segments
+# dim = 2 #number of coordinates
+# delta = 0.01
+# from traj_optimization.traj import traj_opt,traj_show
+# regular_coeff =traj_opt(duration_,stp,dstp,ddstp,fp)
+
+# traj_show(duration_,len(stp),regular_coeff)
